4ab304dd1b62f5751595b96499b29ae097263b28
Sinan Muhammed <sinan9539391118@gmail.com>
2024-07-28 20:40:25 +0530

    init

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import Register from './components/Register';
+import Login from './components/Login';
+import Home from './components/Home';
+import Layout from './components/Layout';
+import Editor from './components/Editor';
+import Admin from './components/Admin';
+import Missing from './components/Missing';
+import Unauthorized from './components/Unauthorized';
+import Lounge from './components/Lounge';
+import LinkPage from './components/LinkPage';
+import RequireAuth from './components/RequireAuth';
+import PersistLogin from './components/PersistLogin';
+import { Routes, Route } from 'react-router-dom';
+
+function App() {
+
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        {/* public routes */}
+        
+        <Route path="linkpage" element={<LinkPage />} />
+        <Route path="unauthorized" element={<Unauthorized />} />
+
+        {/* we want to protect these routes */}
+        <Route element={<PersistLogin />}>
+          <Route path="login" element={<Login />} />
+          <Route path="register" element={<Register />} />
+          <Route element={<RequireAuth />}>
+            <Route path="/" element={<Home />} />
+          </Route>
+        </Route>
+
+        {/* catch all */}
+        <Route path="*" element={<Missing />} />
+      </Route>
+    </Routes>
+  );
+}
+
+export default App;
