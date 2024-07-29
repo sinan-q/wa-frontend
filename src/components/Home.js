5a8f6123ceba525362c6204224673b366f8a8e4e
Sinan Muhammed <sinan9539391118@gmail.com>
2024-07-29 20:40:20 +0530

    Home redesign

--- src/components/Home.js
+++ src/components/Home.js
@@ -59,6 +59,7 @@ const Home = () => {
             const response = await axiosPrivate.post('/wa/start', {
                 withCredentials: true,
             });
+            toast(response.data.message)
             getStatus()
         } catch (err) {
             console.error(err);      
@@ -78,28 +79,40 @@ const Home = () => {
         
 
     return (
-        <div>
-            {status === 0 && <div className="flexGrow">
-                <button onClick={startWA}>Connect Whatsapp</button>
+        <div className="absolute    ">
+            <div className='text-3xl text-center'>Send Message</div>
+
+            {status === 0 && <div className="">
+                <button className="mt-8 text-center w-full bg-black hover:bg-white hover:text-black border hover:border-black p-2 px-4 text-white  " onClick={startWA}>Connect Whatsapp</button>
             </div>}
-            {status === 2 && <div className="flexGrow">
-                <input
-                    type="number"
-                    id="phoneNumber"
-                    autoComplete="off"
-                    onChange={(e) => setNumber(e.target.value)}
-                    value={number}
-                    required />
-                <textarea
-                    onChange={(e) => setMessage(e.target.value)}
-                    value={message}
-                    required>
-                </textarea>
-                <button onClick={sendWA}>Sent</button>
-                <button onClick={stopWA}>Stop</button>
+            {status === 2 && <div className=" p-2 flex flex-col">
+                <label className='mt-3'htmlFor="phoneNumber">Sender Phone Number:</label>
+                <div className='border border-black'>
+                    <label for='username' className=' p-2  inline mr-2'>+91</label>
+                    <input
+                        className='p-1 w-60'
+                        type="number"
+                        id="phoneNumber"
+                        autoComplete="off"
+                        onChange={(e) => setNumber(e.target.value)}
+                        value={number}
+                        required
+                    />  
+                </div>
+                <div className="">
+                    <label className='mt-3 block'  htmlFor="phoneNumber">Message:</label>
+                    <textarea
+                        className="p-1 h-24 border border-black w-full text-sm "
+                        onChange={(e) => setMessage(e.target.value)}
+                        value={message}
+                        required>
+                    </textarea>
+                </div>
+                <button className="text-center bg-black hover:bg-white hover:text-green-500 border hover:border-black p-2 px-4 text-white" onClick={sendWA}>Send</button>
+                <button className=" mt-10 border p-2 text-center w-full hover:bg-red-400 hover:text-white " onClick={stopWA}>Stop</button>
             </div>}
-            <div className="flexGrow">
-                <button onClick={signOut}>Sign Out</button>
+            <div className="mt-1   ">
+                <button className=" border p-2 m-auto text-center w-full hover:bg-red-400 hover:text-white "onClick={signOut}>Sign Out</button>
             </div>
         </div>
     )
